861aaac8b68d33e5d2f0be4298f91093a45f1265
Stephan Gladio <147538067+Stephangladio@users.noreply.github.com>
2024-06-05 18:20:07 +0530

    Updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ export default function Resume() {
         <div className='md:w-1/2 flex justify-center font-hero-font'>
             <div className='flex flex-col justify-center text-white'>
             <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume</h1>
-            <p className='pb-5'>You can view my resume <a className='btn' href="https://drive.google.com/file/d/1NVojy3T8fOtiFnfBvSP8745OGINm_sYI/view?usp=drive_link" target='_blank'>Download</a> </p>
+            <p className='pb-5'>You can view my resume <a className='btn' href="https://drive.google.com/file/d/1xxwuWWd24EMj4SSiQ9ipndK9GvziXv9b/view" target='_blank'>Download</a> </p>
             </div>
         </div>
     </section>
